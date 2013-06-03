a23cb8e3342d8c62f06bdd2ae280f9b9ff4cdb00
Lucas <Lucas@matrix>
2013-06-02 23:35:52 -0300

    Versão + Testes

---
+++ TesteUltimaVersao/test/model/PessoaTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lucas
+ */
+public class PessoaTest {
+    
+    public PessoaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+}
